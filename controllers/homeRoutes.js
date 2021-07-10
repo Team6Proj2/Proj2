5a9f4c7c8c8d7382da5876f05067bb60f60d1fc4
Tyree Nettles <nettles.code@gmail.com>
2021-07-10 11:09:48 -0500

    updated main.handlebars 7/10

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Expense, User, Category } = require("../models");
+const { Expense, User } = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
