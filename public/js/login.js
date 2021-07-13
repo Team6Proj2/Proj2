25a93eb3b2f8a5804ddf947247a548921a7cb6d0
Michael Rao <jmichaelrao@gmail.com>
2021-07-12 21:24:31 -0500

    working logout and login buttons that direct to the correct pages

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = (event) => {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        document.location.replace(`/expenses/${data.user.id}`);
+        document.location.replace("/");
       })
       .catch((err) => {
         res.json(err);
